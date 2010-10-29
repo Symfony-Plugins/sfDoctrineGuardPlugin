8296a06ab9d833255680b60bb4e4b717cb7a2847
gimler <gimler@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-10-29 01:56:28 -0500

    sfDoctrineGuardPlugin[1.3]: remove flash translation in action you can do it in the template my mistake;move mailer call into function
    git-svn-id: http://svn.symfony-project.com/plugins/sfDoctrineGuardPlugin/trunk@31276 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/sfGuardForgotPassword/lib/BasesfGuardForgotPasswordActions.class.php
+++ modules/sfGuardForgotPassword/lib/BasesfGuardForgotPasswordActions.class.php
@@ -24,8 +24,6 @@ abstract class BasesfGuardForgotPasswordActions extends sfActions
 
     if ($request->isMethod('post'))
     {
-      $i18n = $this->getContext()->getI18N();
-
       $this->form->bind($request->getParameter($this->form->getName()));
       if ($this->form->isValid())
       {
@@ -39,21 +37,36 @@ abstract class BasesfGuardForgotPasswordActions extends sfActions
         $forgotPassword->expires_at = new Doctrine_Expression('NOW()');
         $forgotPassword->save();
 
-        $message = $this->getMailer()->compose(
-          sfConfig::get('app_sf_guard_plugin_default_from_email', 'from@noreply.com'),
-          $this->user->email_address,
-          $i18n->__('Forgot Password Request for %name%', array('%name%' => $this->user->username), 'sf_guard'),
-          $this->getPartial('sfGuardForgotPassword/send_request', array('user' => $this->user, 'forgot_password' => $forgotPassword))
-        )->setContentType('text/html');
-        $this->getMailer()->send($message);
+        $this->sendRequestMail($this->user, $forgotPassword);
 
-        $this->getUser()->setFlash('notice', $i18n->__('Check your e-mail! You should receive something shortly!'));
+        $this->getUser()->setFlash('notice', 'Check your e-mail! You should receive something shortly!');
 
         $this->redirect(sfConfig::get('app_sf_guard_plugin_password_request_url', '@sf_guard_signin'));
       } else {
-        $this->getUser()->setFlash('error', $i18n->__('Invalid e-mail address!'));
+        $this->getUser()->setFlash('error', 'Invalid e-mail address!');
       }
     }
+  }
+
+  /**
+   * Send the request password email to the user
+   *
+   * @param object                $user           the user object
+   * @param sfGuardForgotPassword $forgotPassword the forgot password record
+   *
+   * @return void
+   */
+  public function sendRequestMail($user, $forgotPassword)
+  {
+    $i18n = $this->getContext()->getI18N();
+
+    $message = $this->getMailer()->compose(
+      sfConfig::get('app_sf_guard_plugin_default_from_email', 'from@noreply.com'),
+      $this->user->email_address,
+      $i18n->__('Forgot Password Request for %name%', array('%name%' => $this->user->username), 'sf_guard'),
+      $this->getPartial('sfGuardForgotPassword/send_request', array('user' => $this->user, 'forgot_password' => $forgotPassword))
+    )->setContentType('text/html');
+    $this->getMailer()->send($message);
   }
 
   public function executeChange($request)
@@ -71,21 +84,34 @@ abstract class BasesfGuardForgotPasswordActions extends sfActions
 
         $this->_deleteOldUserForgotPasswordRecords();
 
-        $i18n = $this->getContext()->getI18N();
+        $this->sendChangeMail($this->user, $request['sf_guard_user']['password']);
 
-        $message = $this->getMailer()->compose(
-          sfConfig::get('app_sf_guard_plugin_default_from_email', 'from@noreply.com'),
-          $this->user->email_address,
-          $i18n->__('New Password for %name%', array('%name%' => $this->user->username) , 'sf_guard'),
-          $this->getPartial('sfGuardForgotPassword/new_password', array('user' => $this->user, 'password' => $request['sf_guard_user']['password']))
-        )->setContentType('text/html');
-        $this->getMailer()->send($message);
-
-        $this->getUser()->setFlash('notice', $i18n->__('Password updated successfully!'));
+        $this->getUser()->setFlash('notice', 'Password updated successfully!');
 
         $this->redirect('@sf_guard_signin');
       }
     }
+  }
+
+  /**
+   * Send email to the user with new password
+   *
+   * @param object $user     user object
+   * @param string $password user password
+   *
+   * @return void
+   */
+  protected function sendChangeMail($user, $password)
+  {
+    $i18n = $this->getContext()->getI18N();
+
+    $message = $this->getMailer()->compose(
+      sfConfig::get('app_sf_guard_plugin_default_from_email', 'from@noreply.com'),
+      $user->email_address,
+      $i18n->__('New Password for %name%', array('%name%' => $user->username) , 'sf_guard'),
+      $this->getPartial('sfGuardForgotPassword/new_password', array('user' => $user, 'password' => $password))
+    )->setContentType('text/html');
+    $this->getMailer()->send($message);
   }
 
   private function _deleteOldUserForgotPasswordRecords()
