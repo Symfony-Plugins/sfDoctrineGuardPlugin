9753a24a6b549cb71e4a6a2cf394850828fd45a9
gimler <gimler@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-11-06 11:12:38 -0500

    sfDoctrineGuardPlugin[1.4]: use sfWidgetFormInputText instate of old sfWidgetFormInput
    git-svn-id: http://svn.symfony-project.com/plugins/sfDoctrineGuardPlugin/trunk@31328 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/form/base/BasesfGuardRequestForgotPasswordForm.class.php
+++ lib/form/base/BasesfGuardRequestForgotPasswordForm.class.php
@@ -12,7 +12,7 @@ class BasesfGuardRequestForgotPasswordForm extends BaseForm
 {
   public function setup()
   {
-    $this->widgetSchema['email_address'] = new sfWidgetFormInput();
+    $this->widgetSchema['email_address'] = new sfWidgetFormInputText();
     $this->validatorSchema['email_address'] = new sfGuardValidatorUsernameOrEmail(
       array('trim' => true),
       array('required' => 'Your username or e-mail address is required.', 'invalid' => 'Username or e-mail address not found please try again.')
